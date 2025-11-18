291dc1cdc57ff14484ab80503f10a7e5df372165
Joni Mäkinen <joni-makinen@live.fi>
2025-11-19 00:26:41 +0200

    Modify integration testing text string

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from agents.skill_assessor import SkillAssessor
 
 def main():
     assessor = SkillAssessor()
-    result = assessor.assess("")
+
+    text = """
+    Joni strated programming in 2020.
+    He is a developer with 2 years of experience with JavaScript, and half a year with Python.
+    He is very familiar with Node.js, and fairly familiar with LangChain and OpenAI Agents.
+    He has built multiple full-stack apps over the years.
+    """
+    result = assessor.assess(text)
     print(result)
 
 if __name__ == "__main__":
