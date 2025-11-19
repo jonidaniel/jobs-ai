fffc5a16f006b540d33baa52392352299f8f82f1
Joni Mäkinen <joni-makinen@live.fi>
2025-11-19 20:56:25 +0200

    Add 'from .skill_assessor import SkillAssessor'

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,4 @@
 # Allows importing with: from agents import ...
 from .prompts.skill_assessor_prompts import SYSTEM_PROMPT, USER_PROMPT_TEMPLATE, OUTPUT_SCHEMA_INSTRUCTION
 from .schemas.skill_profile import SkillProfile
+from .skill_assessor import SkillAssessor
