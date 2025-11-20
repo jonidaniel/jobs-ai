23c646463cb87266c22bf78a2f2aaf0718f0d35d
Joni Mäkinen <joni-makinen@live.fi>
2025-11-20 09:31:15 +0200

    Add file header and plan rest of agents in comments

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-from agents import SkillAssessor, SearcherAgent
+# ---------- JOBSAI MAIN ----------
+
+from agents import AssessorAgent, SearcherAgent, INPUT_TEXT#, ScorerAgent, ReporterAgent, GeneratorAgent, NotifierAgent
 
 def main():
-    assessor = SkillAssessor()
+    #planner = PlannerAgent()
+    assessor = AssessorAgent()
     searcher = SearcherAgent()
+    #scorer = ScorerAgent()
+    #reporter = ReporterAgent()
+    #generator = GeneratorAgent()
+    #notifier = NotifierAgent()
 
-    text = """
-    Joni Mäkinen has developed software since 2020.
-    He has built and published multiple full-stack apps (frontend, backend, database, desktop, mobile).
-    He has 3 years of experience with Git.
-    He has 2 years of experience with web development.
-    He has 2 years of experience with Node.js development.
-    He has 2 years of experience with JavaScript.
-    He has 1.5 years of experience with SQL.
-    He has 1 year of experience with AWS.
-    He has 1 year of experience with Python.
-    He has 1 year of experience with React.
-    He has little experience with React Native.
-    He has little experience with Java, Kotlin, C++, and C#.
-    He has little experience with LangChain, OpenAI Agents, and CrewAI.
-    He has very good soft skills.
-    He is an AWS Certified Solutions Architect (SAA-C03).
-    He is an AWS Certified Cloud Practitioner (CLF-C02).
-    """
-
-    assessment = assessor.assess(text)
+    assessment = assessor.assess(INPUT_TEXT)
     searches = searcher.search_jobs(assessment.model_dump())
-    print(searches)
-
-    #assessor.save(result)
-    #loaded = assessor.load_existing()
-    #print(loaded == result)
-
-    #loaded = assessor.load_existing()
-    #print(loaded)
+    #print(searches)
 
 if __name__ == "__main__":
     main()
