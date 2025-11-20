076d5e75686bb78b68a0abfff654d6c860c1b414
Joni Mäkinen <joni-makinen@live.fi>
2025-11-20 19:05:28 +0200

    Pass configuration constants when initializing agents

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 # enter your skills and preferences to the system once and get job recommendations and cover letters delivered to you continuously.
 # Checks input resources consistently and updates search queries autonomously.
 
+from dotenv import load_dotenv
+
 from agents import (
     AssessorAgent,
     SearcherAgent,
@@ -16,31 +18,46 @@ from agents import (
     )
 
 from config import (
-    USER_INPUT, # Initial skill and experience description of a candidate
-    SKILL_PROFILE_PATH
+    USER_PROMPT,
+    OPENAI_MODEL,
+    OPENAI_API_KEY,
+    SKILL_PROFILE_PATH,
+    JOB_BOARDS,
+    DEEP_MODE,
+    JOB_LISTINGS_RAW_PATH,
+    JOB_LISTINGS_SCORED_PATH,
+    REPORTS_PATH
     )
 
 def main():
+    load_dotenv()
+
+    assessor = AssessorAgent(OPENAI_MODEL, OPENAI_API_KEY, SKILL_PROFILE_PATH)
+    searcher = SearcherAgent(JOB_BOARDS, DEEP_MODE)
+    scorer = ScorerAgent(JOB_LISTINGS_RAW_PATH, JOB_LISTINGS_SCORED_PATH)
+    reporter = ReporterAgent(JOB_LISTINGS_SCORED_PATH, REPORTS_PATH)
+
     # 1. Assess candidate
     # Returns a SkillProfile object
-    skill_profile = AssessorAgent().assess(USER_INPUT)
+    skill_profile = assessor.assess(USER_PROMPT)
+    print("Skill assessment complete")
 
     # 2. Search jobs based on assessment
     # Uses skill_profile to form keyword searches
     # The keyword searches are then used to scrape popular job listing websites
     # Stores acquired job listings as JSON to /data/job_listings/raw/*.json
-    SearcherAgent(job_boards=["duunitori"], deep=True).search_jobs(skill_profile.model_dump())
+    searcher.search_jobs(skill_profile.model_dump())
     print("Searching job listings complete. Listings saved in /data/job_listings/")
 
     # 3. Score the jobs
     # Loads raw job listings JSON from /data/job_listings/raw/*.json
     # Saves scored job listings as JSON to /data/job_listings/scored/*.json
-    ScorerAgent().score_jobs(skill_profile=skill_profile)
+    scorer.score_jobs(skill_profile=skill_profile)
     print("Scoring jobs complete. Scored jobs saved in /data/job_listings/scored/")
 
     # 4. Write a job listing report
     # Saves the report to /data/reports/job_report.txt
-    ReporterAgent().generate_report(top_n=10)
+    reporter.generate_report(top_n=10)
     print("Writing job listing report complete. Report saved to /data/reports/job_report.txt successfully")
 
 if __name__ == "__main__":
