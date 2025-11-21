af39e4716ae71f096b2708b91c1e739b292078a8
Joni Mäkinen <joni-makinen@live.fi>
2025-11-21 15:41:31 +0200

    Add file with Duunitori headers

---
+++ config/headers.py
@@ -0,0 +1,12 @@
+# ---------- DUUNITORI HEADERS ----------
+
+HEADERS_DUUNITORI = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/122.0.0.0 Safari/537.36"
+    ),
+    "Accept-Language": "fi-FI,fi;q=0.9,en;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Connection": "keep-alive"
+}
