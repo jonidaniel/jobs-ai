7cc1c0f8c0ff09326a5e01e4c442f1949d3526ae
Joni Mäkinen <joni-makinen@live.fi>
2025-11-25 15:15:58 +0200

    Add LETTER_STYLE constant, add descriptions and possible options to all settings

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,11 @@
 # ---------- SETTINGS ----------
 
-JOB_BOARDS = ["Duunitori"]
-DEEP_MODE = True  # Set to False if there's no need for full job descriptions when scraping job listings
+# The job boards to scrape for job listings
+JOB_BOARDS = ["Duunitori"]  # Choose from "Duunitori" and "Jobly"
+# Deep mode allows deeper scraping of job listings
+# If True, the listings are opened individually, i.e., each one is crawled for full job description
+DEEP_MODE = True  # Choose from True or False
+# The size of the job report
+REPORT_SIZE = 10
+# The style or tone of the cover letters
+LETTER_STYLE = "professional"  # Choose from "professional", "friendly", or "confident"
