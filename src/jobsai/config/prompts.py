5227e45db83a9f5e32d61a795de9277ceecac05a
Joni Mäkinen <joni-makinen@live.fi>
2025-11-28 11:38:04 +0200

    Comment out parts of prompt (so that new real prompt can be tested in ProfilerAgent)

--- src/jobsai/config/prompts.py
+++ src/jobsai/config/prompts.py
@@ -21,26 +21,26 @@ Normalize skill names (e.g., "js" -> "JavaScript", "py" -> "Python").
 Be concise and avoid duplicates.
 """
 
-USER_INPUT = """
-Joni Mäkinen has developed software since 2020.
-He has built and published multiple full-stack apps (frontend, backend, database, desktop, mobile).
-He has 3 years of experience with Git.
-He has 2 years of experience with web development.
-He has 2 years of experience with Node.js development.
-He has 2 years of experience with JavaScript.
-He has 1.5 years of experience with SQL.
-He has 1 year of experience with AWS.
-He has 1 year of experience with Python.
-He has 1 year of experience with React.
-He has 0.5 years of experience with Scikit-learn.
-He has 0.5 years of experience with PyTorch.
-He has little experience with React Native.
-He has little experience with Java, Kotlin, C++, and C#.
-He has little experience with LangChain, OpenAI Agents, and CrewAI.
-He has very good soft skills.
-He is an AWS Certified Solutions Architect (SAA-C03).
-He is an AWS Certified Cloud Practitioner (CLF-C02).
-"""
+# USER_INPUT = """
+# Joni Mäkinen has developed software since 2020.
+# He has built and published multiple full-stack apps (frontend, backend, database, desktop, mobile).
+# He has 3 years of experience with Git.
+# He has 2 years of experience with web development.
+# He has 2 years of experience with Node.js development.
+# He has 2 years of experience with JavaScript.
+# He has 1.5 years of experience with SQL.
+# He has 1 year of experience with AWS.
+# He has 1 year of experience with Python.
+# He has 1 year of experience with React.
+# He has 0.5 years of experience with Scikit-learn.
+# He has 0.5 years of experience with PyTorch.
+# He has little experience with React Native.
+# He has little experience with Java, Kotlin, C++, and C#.
+# He has little experience with LangChain, OpenAI Agents, and CrewAI.
+# He has very good soft skills.
+# He is an AWS Certified Solutions Architect (SAA-C03).
+# He is an AWS Certified Cloud Practitioner (CLF-C02).
+# """
 
 USER_PROMPT_BASE = """
 Below is the INPUT TEXT describing the candidate's background.
@@ -63,6 +63,6 @@ Now produce a structured skill profile following the JSON schema provided below.
 """
 
 # Inject the user prompt and the output schema into the user prompt base to create the final prompt
-USER_PROMPT = USER_PROMPT_BASE.format(
-    user_input=USER_INPUT, output_schema=OUTPUT_SCHEMA
-)
+# USER_PROMPT = USER_PROMPT_BASE.format(
+#     user_input=USER_INPUT, output_schema=OUTPUT_SCHEMA
+# )
