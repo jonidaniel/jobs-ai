1b190893fccf9cc92d92e2f6a337bd9e87369670
Joni Mäkinen <joni-makinen@live.fi>
2025-12-02 20:53:50 +0200

    Modify frontpage header

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../styles/hero.css";
 export default function Hero() {
   return (
     <section id="hero">
-      <h1>JobsAI-ITs</h1>
+      <h1>JobsAI'ITs</h1>
       <p>Let us do your job searching for you</p>
     </section>
   );
